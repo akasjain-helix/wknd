7d8a28a739ca3897e2e59e856d72f299b2d97df0
akasjain <akasjain@adobe.com>
2025-03-12 15:54:27 +0530

    submit handler

--- blocks/subscribe/subscribe.js
+++ blocks/subscribe/subscribe.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
     const link = block.children[0].querySelector('a');
-
+    const classList = block.classList;
     const form = document.createElement('form');
     form.action = link.getAttribute('href');
-    form.method = 'get';
+    form.method = classList.contains('post') ? 'post' : 'get';
     //form.target = '_blank';
     form.style = 'display: flex; align-items: center;';
     const input = document.createElement('input');
@@ -13,4 +13,24 @@ export default function decorate(block) {
     form.append(input);
     block.textContent = '';
     block.append(form);
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent redirection
+
+        // Fetch form data
+        //const formData = new FormData(this);
+
+        // Send form data using fetch API
+        fetch(this.action, {
+            method: this.method,
+            body: {}
+        })
+            .then(response => response.json()) // Adjust based on expected response type
+            .then(data => {
+                console.log("Form submitted successfully:", data);
+            })
+            .catch(error => {
+                console.error("Error submitting form:", error);
+            });
+    });
 };
