56797d55a922ff07ba4d9d5471c985e50e0d08e4
akasjain <akasjain@adobe.com>
2025-03-20 23:48:10 +0530

    Improve style for cors example

--- blocks/subscribe-fetch/subscribe-fetch.js
+++ blocks/subscribe-fetch/subscribe-fetch.js
@@ -1,23 +1,16 @@
 export default function decorate(block) {
     const link = block.children[0].querySelector('a');
     const classList = block.classList;
-    const form = document.createElement('form');
-    form.action = link.getAttribute('href');
-    form.method = classList.contains('post') ? 'post' : 'get';
-    //form.target = '_blank';
-    form.style = 'display: flex; align-items: center;';
-    const input = document.createElement('input');
-    input.type = 'submit';
-    input.value = link.innerText.trim();
-    input.style = 'background-color: #f1f1f1; border: none; padding: 10px;';
-    form.append(input);
+    const btn = document.createElement('input');
+    btn.type = 'submit';
+    btn.value = link.innerText.trim();
     block.textContent = '';
-    block.append(form);
+    block.append(btn);
 
-    form.addEventListener("submit", function(event) {
+    btn.addEventListener("click", function(event) {
         event.preventDefault();
-        fetch(this.action, {
-                method: this.method,
+        fetch(link, {
+                method: classList.contains('post') ? 'post' : 'get',
                 //credentials: 'include',
                 //mode: "no-cors",
                 Headers: {
