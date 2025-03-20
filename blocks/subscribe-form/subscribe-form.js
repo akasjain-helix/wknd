85cb98ca2500039e28c034f57ca1653015489b1b
akasjain <akasjain@adobe.com>
2025-03-20 14:25:15 +0530

    form fix

--- blocks/subscribe-form/subscribe-form.js
+++ blocks/subscribe-form/subscribe-form.js
@@ -11,10 +11,15 @@ export default function decorate(block) {
     form.method = httpMethod;
     //form.target = '_blank';
     form.style = 'display: flex; align-items: center;';
+    const label = document.createElement('label');
+    label.value = "Click me";
+    label.style = 'display: none';
+
     const input = document.createElement('input');
     input.type = 'submit';
     input.value = link.innerText.trim();
     input.style = 'background-color: #f1f1f1; border: none; padding: 10px;';
+    form.append(label);
     form.append(input);
     block.textContent = '';
     block.append(form);
